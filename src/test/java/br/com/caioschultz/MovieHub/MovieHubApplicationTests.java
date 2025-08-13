70fadb4527a2599bc455bfa74ad53e83c20facbb
Caio Mazziero Schultz <caio.mazziero.schultz@gmail.com>
2025-08-13 14:02:47 -0300

    First commit

---
+++ src/test/java/br/com/caioschultz/MovieHub/MovieHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caioschultz.MovieHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
