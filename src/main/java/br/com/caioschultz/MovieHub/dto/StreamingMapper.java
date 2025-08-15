e221afe0778917a630032fddcbce9ce7e8fd2268
Caio Mazziero Schultz <caio.mazziero.schultz@gmail.com>
2025-08-15 14:39:49 -0300

    feat: cria StreamingMapper

---
+++ src/main/java/br/com/caioschultz/MovieHub/dto/StreamingMapper.java
@@ -0,0 +1,26 @@
+package br.com.caioschultz.MovieHub.dto;
+
+import br.com.caioschultz.MovieHub.controller.request.StreamingRequest;
+import br.com.caioschultz.MovieHub.controller.response.StreamingResponse;
+import br.com.caioschultz.MovieHub.entity.Streaming;
+import lombok.experimental.UtilityClass;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StreamingMapper {
+
+
+    public Streaming toStreaming(StreamingRequest streamingRequest){
+        Streaming streaming = new Streaming();
+        streaming.setName(streamingRequest.name());
+
+        return streaming;
+    }
+
+    public StreamingResponse toResponse(Streaming streaming){
+
+        StreamingResponse response = new StreamingResponse(streaming.getId(), streaming.getName());
+
+        return response;
+    }
+}
